e9c553a3e6c523881a132cacacf1dfb029c7ae76
maria_edurada <mariaduda.mendonca.m@gmail.com>
2023-12-01 16:26:34 -0300

    adicionando popup de perfil funcionando

--- tela_principal/js/script.js
+++ tela_principal/js/script.js
@@ -8,3 +8,21 @@ function updateClock() {
   }
   updateClock();
   setInterval(updateClock, 1000);
+
+  var popup = document.getElementById('popup');
+  var perfil = document.getElementById('perfil');
+  
+  perfil.addEventListener('click', function() {
+    if (popup.style.display === 'none' || popup.style.display === '') {
+      popup.style.display = 'block';
+      perfil.focus();
+    } else {
+      popup.style.display = 'none';
+    }
+  });
+  
+  document.addEventListener('click', function(event) {
+    if (event.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
