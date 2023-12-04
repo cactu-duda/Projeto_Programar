b4b0b64d161a328415991acf3815bac1bb33e70a
May <151782471+Mayippie@users.noreply.github.com>
2023-12-04 17:35:11 -0300

    adicionando janela portfolio

--- tela_principal/js/script.js
+++ tela_principal/js/script.js
@@ -25,9 +25,30 @@ function fecharPopup(event) {
     }
 }
 
+function abrirPasta() {
+    var portfolio = document.getElementById('portfolio');
+    var flavia = document.getElementById('flavia');
+    if (portfolio.style.display === 'none' || portfolio.style.display === '') {
+        portfolio.style.display = 'block';
+        flavia.focus();
+    } else {
+        portfolio.style.display = 'none';
+    }
+}
+
+function fecharPasta(event) {
+    var portfolio = document.getElementById('portfolio');
+    if (event.target === portfolio) {
+        portfolio.style.display = 'none';
+    }
+}
+
 
 updateClock();
 setInterval(updateClock, 60000);
 
 document.getElementById('perfil').addEventListener('click', abrirPopup);
 document.addEventListener('click', fecharPopup);
+
+document.getElementById('flavia').addEventListener('click', abrirPasta);
+document.addEventListener('click', fecharPasta);
