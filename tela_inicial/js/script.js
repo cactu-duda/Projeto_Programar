91a8907caa99218cbd0de4e0c1cb5dd911817919
Maria Eduarda <151681573+cactu-duda@users.noreply.github.com>
2023-12-05 10:00:24 -0300

    adicionado js login

--- tela_inicial/js/script.js
+++ tela_inicial/js/script.js
@@ -1,1 +1,12 @@
+function logar(){
+    var email = document.getElementById("email");
+    var senha = document.getElementById("senha")
 
+    console.log(email.value+senha.value)
+
+    if(email == "valdinei@pp.com" && senha.value == "programar"){
+        localStorage.setItem("acesso", true);
+
+        window.location.href = "tela_principal.html"
+    }else alert("Usuário ou senha inválido")
+}
